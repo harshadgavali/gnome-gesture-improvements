8632a2fd17e2777624ec310771dc253ffc3d0602
Harshad Gavali <harshadgavali@outlook.com>
2021-07-16 19:52:11 +0530

    Preserver comments when transpiling

--- scripts/transpile.ts
+++ scripts/transpile.ts
@@ -57,18 +57,24 @@ function createVariableStatement(
 	);
 }
 
+function moveComments<T extends ts.Node>(node: T, originalNode: ts.Node): T {
+	node = ts.setSyntheticLeadingComments(node, ts.getSyntheticLeadingComments(originalNode));
+	node = ts.setSyntheticTrailingComments(node, ts.getSyntheticTrailingComments(originalNode));
+	return ts.setCommentRange(node, ts.getCommentRange(originalNode));
+}
+
 const printer: ts.Printer = ts.createPrinter({ removeComments: false });
 
 const transformExports: ts.TransformerFactory<ts.SourceFile> = context => {
-	const tranformFunction = (node: ts.FunctionDeclaration, variables: string[]): ts.FunctionDeclaration | ts.VariableStatement => {
+	const tranformFunction = (node: ts.FunctionDeclaration, variables: string[]): ts.FunctionDeclaration => {
 		if (!node.modifiers?.some(m => m.kind === ts.SyntaxKind.ExportKeyword)) {
 			return node;
 		}
 
 		variables.push(node.name?.text || '');
-		return createVariableStatement(context,
-			node.name?.text || '',
-			context.factory.createFunctionExpression(
+		return moveComments(
+			context.factory.createFunctionDeclaration(
+				node.decorators,
 				node.modifiers.filter(m => m.kind !== ts.SyntaxKind.ExportKeyword),
 				node.asteriskToken,
 				node.name,
@@ -77,6 +83,7 @@ const transformExports: ts.TransformerFactory<ts.SourceFile> = context => {
 				node.type,
 				node.body || context.factory.createBlock([])
 			),
+			node
 		);
 	};
 
@@ -86,16 +93,19 @@ const transformExports: ts.TransformerFactory<ts.SourceFile> = context => {
 		}
 
 		variables.push(node.name?.text || '');
-		return createVariableStatement(context,
-			node.name?.text || '',
-			context.factory.createClassExpression(
-				node.decorators,
-				node.modifiers.filter(m => m.kind !== ts.SyntaxKind.ExportKeyword),
-				node.name,
-				node.typeParameters,
-				node.heritageClauses,
-				node.members
-			)
+		return moveComments(
+			createVariableStatement(context,
+				node.name?.text || '',
+				context.factory.createClassExpression(
+					node.decorators,
+					node.modifiers.filter(m => m.kind !== ts.SyntaxKind.ExportKeyword),
+					node.name,
+					node.typeParameters,
+					node.heritageClauses,
+					node.members
+				)
+			),
+			node
 		);
 	};
 
@@ -109,7 +119,7 @@ const transformExports: ts.TransformerFactory<ts.SourceFile> = context => {
 				if (d.name.kind == ts.SyntaxKind.Identifier) {
 					variables.push((d.name as ts.Identifier).text);
 				}
-				return createVariableDeclaration(context, d.name, d.initializer);
+				return moveComments(createVariableDeclaration(context, d.name, d.initializer), d);
 			})
 		);
 	};
@@ -117,60 +127,57 @@ const transformExports: ts.TransformerFactory<ts.SourceFile> = context => {
 	return sourceFile => {
 		const variables: string[] = [];
 		const visitor = (node: ts.Node): ts.Node => {
-			// console.log(node.kind, `\t# ${ts.SyntaxKind[node.kind]}`);
-
-			let varNode: ts.Node;
 			switch (node.kind) {
 			case ts.SyntaxKind.ClassDeclaration:
-				varNode = transformClass(node as ts.ClassDeclaration, variables);
-				// console.log(printer.printNode(ts.EmitHint.Unspecified, varNode, sourceFile));
-				return varNode;
+				return transformClass(node as ts.ClassDeclaration, variables);
 			case ts.SyntaxKind.FunctionDeclaration:
-				varNode = tranformFunction(node as ts.FunctionDeclaration, variables);
-				// console.log(printer.printNode(ts.EmitHint.Unspecified, varNode, sourceFile));
-				return varNode;
+				return tranformFunction(node as ts.FunctionDeclaration, variables);
 			case ts.SyntaxKind.VariableStatement:
-				varNode = tranformVariable(node as ts.VariableStatement, variables);
-				// console.log(printer.printNode(ts.EmitHint.Unspecified, varNode, sourceFile));
-				return varNode;
+				return tranformVariable(node as ts.VariableStatement, variables);
+			default:
+				return node;
 			}
-
-			return node;
 		};
-		sourceFile = ts.visitEachChild(sourceFile, visitor, context);
+		const modifiedSourceFile = ts.visitEachChild(sourceFile, visitor, context);
+		if (variables.length && modifiedSourceFile.statements.length) {
 
-		if (variables.length) {
 			ts.addSyntheticLeadingComment(
-				sourceFile,
+				modifiedSourceFile.statements[0],
 				ts.SyntaxKind.MultiLineCommentTrivia,
-				`exported ${variables.join(',')}`,
+				` exported ${variables.join(', ')} `,
 				true
 			);
 		}
-		return sourceFile;
+
+		return moveComments(modifiedSourceFile, sourceFile);
 	};
 };
 
 // local, gi and shell imports
 const transformImports: ts.TransformerFactory<ts.SourceFile> = context => {
 
-	const transformImport = (node: ts.ImportDeclaration, getModuleReplacement: (module: string) => { statement?: ts.VariableStatement, module: ts.Expression } | null): ts.ImportDeclaration | ts.VariableStatement[] => {
+	const transformImport = (
+		node: ts.ImportDeclaration,
+		getModuleReplacement: (module: string) => {
+			statement?: ts.VariableStatement,
+			module: ts.Expression
+		} | null
+	): ts.ImportDeclaration | ts.VariableStatement[] | ts.EmptyStatement => {
+
 		const module = node.moduleSpecifier as ts.StringLiteral;
 		if (module.text === 'gnome-shell') {
-			return [];
+			return moveComments(context.factory.createEmptyStatement(), node);
 		}
 		const replacement = getModuleReplacement(module.text);
 
 		if (!replacement) {
 			return node;
 		}
-		// module.flags
+
 		const statements: ts.VariableStatement[] = [];
 		if (replacement.statement) {
 			statements.push(replacement.statement);
 		}
-
-		// console.log(node.importClause);
 
 		if (node.importClause?.name) {
 			statements.push(createVariableStatement(context,
@@ -219,7 +226,12 @@ const transformImports: ts.TransformerFactory<ts.SourceFile> = context => {
 			));
 		}
 
-		return statements;
+		if (statements.length) {
+			moveComments(statements[0], node);
+			return statements;
+		}
+
+		return node;
 	};
 
 	return sourceFile => {
@@ -266,7 +278,8 @@ const transformImports: ts.TransformerFactory<ts.SourceFile> = context => {
 
 			return transformImport(node as ts.ImportDeclaration, getModuleReplacement);
 		};
-		return ts.visitEachChild(sourceFile, visitor, context);
+
+		return moveComments(ts.visitEachChild(sourceFile, visitor, context), sourceFile);
 	};
 };
 
@@ -275,33 +288,39 @@ const transformGObjectClasses: ts.TransformerFactory<ts.SourceFile> = context =>
 		if (node.kind === ts.SyntaxKind.CallExpression) {
 			const callNode = node as ts.CallExpression;
 			if (callNode.expression.kind === ts.SyntaxKind.SuperKeyword) {
-				return context.factory.createCallExpression(
-					context.factory.createPropertyAccessExpression(
-						context.factory.createIdentifier('super'),
-						context.factory.createIdentifier('_init')
+				return moveComments(
+					context.factory.createCallExpression(
+						context.factory.createPropertyAccessExpression(
+							context.factory.createIdentifier('super'),
+							context.factory.createIdentifier('_init')
+						),
+						callNode.typeArguments,
+						callNode.arguments
 					),
-					callNode.typeArguments,
-					callNode.arguments
+					node
 				);
 			}
 		}
-		return ts.visitEachChild(node, replaceSuperCall, context);
+		return moveComments(ts.visitEachChild(node, replaceSuperCall, context), node);
 	};
 
 	const transformConstructor = (node: ts.Node): ts.Node => {
 		if (node.kind === ts.SyntaxKind.Constructor) {
 			const constructorNode = node as ts.ConstructorDeclaration;
 
-			return context.factory.createMethodDeclaration(
-				constructorNode.decorators,
-				constructorNode.modifiers,
-				constructorNode.asteriskToken,
-				'_init',
-				constructorNode.questionToken,
-				constructorNode.typeParameters,
-				constructorNode.parameters,
-				constructorNode.type,
-				ts.visitEachChild(constructorNode.body, replaceSuperCall, context)
+			return moveComments(
+				context.factory.createMethodDeclaration(
+					constructorNode.decorators,
+					constructorNode.modifiers,
+					constructorNode.asteriskToken,
+					'_init',
+					constructorNode.questionToken,
+					constructorNode.typeParameters,
+					constructorNode.parameters,
+					constructorNode.type,
+					ts.visitEachChild(constructorNode.body, replaceSuperCall, context)
+				),
+				node
 			);
 		}
 		return node;
@@ -311,7 +330,7 @@ const transformGObjectClasses: ts.TransformerFactory<ts.SourceFile> = context =>
 		const visitor = (node: ts.Node): ts.Node => {
 			if (node.kind === ts.SyntaxKind.CallExpression) {
 				const callNode = node as ts.CallExpression;
-				
+
 				if (callNode.expression.kind === ts.SyntaxKind.Identifier) {
 					// registerClass()
 					if ((callNode.expression as ts.Identifier).text !== 'registerClass')
@@ -320,77 +339,53 @@ const transformGObjectClasses: ts.TransformerFactory<ts.SourceFile> = context =>
 				else if (callNode.expression.kind === ts.SyntaxKind.PropertyAccessExpression) {
 					// GObject.registerClass()
 					const id = callNode.expression as ts.PropertyAccessExpression;
-					// console.log(`id kind: ${ts.SyntaxKind[id.expression.kind]}`);
 					if (id.expression.kind !== ts.SyntaxKind.Identifier || id.name.kind !== ts.SyntaxKind.Identifier) return node;
 					if ((id.name as ts.Identifier).text !== 'registerClass')
 						return node;
 				}
 				else return node;
 
-				// console.log(`tranform classes, registerClass(${callNode.arguments.map(a => ts.SyntaxKind[a.kind]).join(', ')})`);
-				
 				// second argument is class expression
 				if (callNode.arguments.length === 2 && callNode.arguments[1].kind === ts.SyntaxKind.ClassExpression) {
-					return context.factory.createCallExpression(
-						callNode.expression,
-						callNode.typeArguments,
-						[
-							callNode.arguments[0],
-							ts.visitEachChild(callNode.arguments[1], transformConstructor, context)
-						]
+					return moveComments(
+						context.factory.createCallExpression(
+							callNode.expression,
+							callNode.typeArguments,
+							[
+								callNode.arguments[0],
+								moveComments(ts.visitEachChild(callNode.arguments[1], transformConstructor, context), callNode.arguments[1])
+							]
+						),
+						node
 					);
 				}
 				if (callNode.arguments.length === 1 && callNode.arguments[0].kind === ts.SyntaxKind.ClassExpression) {
-					// console.log('calling transformConstructor');
-					
-					return context.factory.createCallExpression(
-						callNode.expression,
-						callNode.typeArguments,
-						[
-							ts.visitEachChild(callNode.arguments[0], transformConstructor, context)
-						]
+					return moveComments(
+						context.factory.createCallExpression(
+							callNode.expression,
+							callNode.typeArguments,
+							[
+								moveComments(ts.visitEachChild(callNode.arguments[0], transformConstructor, context), callNode.arguments[0])
+							]
+						),
+						node
 					);
 				}
-				
 				return node;
 			}
-			return ts.visitEachChild(node, visitor, context);
+			return moveComments(ts.visitEachChild(node, visitor, context), node);
 		};
 
-		return ts.visitEachChild(sourceFile, visitor, context);
+		return moveComments(ts.visitEachChild(sourceFile, visitor, context), sourceFile);
 	};
 };
 
 const matches = new glob.GlobSync('build/src/**/*.js');
 matches.found.forEach(file => {
-	// console.log(file);
+	console.log(file);
 
 	const text = fs.readFileSync(file).toString();
-	const sourceFile = ts.createSourceFile(file, text, ts.ScriptTarget.ES2018);
-	// const sourceFile = program.getSourceFile(file);
-	// sourceFile?.forEachChild(node => {
-	//     console.log(ts.SyntaxKind[node.kind]);
-	//     if (node.kind === ts.SyntaxKind.ClassDeclaration) {
-	//         const classNode = node as ts.ClassDeclaration;
-	//         const exportMod = classNode.modifiers?.filter(mod => mod.kind === ts.SyntaxKind.ExportKeyword);
-	//         if (exportMod) {
-	//             console.log(printer.printNode(ts.EmitHint.Unspecified, node, sourceFile));
-	//         }
-	//     }
-	// })
-
-	const result = ts.transform(sourceFile,
-		[transformExports, transformImports, transformGObjectClasses],
-		{
-			removeComments: false
-		});
-	// result.transformed[0]
-
-	// console.log(result.transformed.length);
-	fs.writeFileSync(file, printer.printFile(result.transformed[0]));
-	// console.log(printer.printFile(result.transformed[0]));
-	// console.log(result.transformed[0].text);
-
-	// console.log();
-
+	let sourceFile = ts.createSourceFile(file, text, ts.ScriptTarget.ES2018, true, ts.ScriptKind.JS);
+	sourceFile = ts.transform(sourceFile, [transformExports, transformImports, transformGObjectClasses]).transformed[0];
+	fs.writeFileSync(file, printer.printFile(sourceFile));
 });
