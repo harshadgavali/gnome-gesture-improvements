1036b013c3fc7dde19af665973a001cb047bf1f0
Harshad Gavali <harshadgavali@outlook.com>
2021-10-23 05:58:49 +0530

    Add environment file for shell related changes

---
+++ extension/src/utils/environment.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import GObject from '@gi-types/gobject';
+import { Actor, AnimationMode } from '@gi-types/clutter';
+import { Adjustment } from '@gi-types/st';
+
+declare type EaseParamsType<T extends GObject.Object> = {
+	duration: number;
+	mode: AnimationMode;
+	repeatCount?: number;
+	autoReverse?: boolean;
+	onStopped?: (isFinished?: boolean) => void;
+} & { [P in KeysOfType<T, number>]?: number };
+
+export function easeActor<T extends Actor>(actor: T, params: EaseParamsType<T>): void {
+	(actor as any).ease(params);
+}
+
+export function easeAdjustment(actor: Adjustment, value: number, params: EaseParamsType<Adjustment>): void {
+	(actor as any).ease(value, params);
+}
