547be74bf499b583422a320fdd1c04adb991dce9
Harshad Gavali <harshadgavali@outlook.com>
2021-10-23 12:36:44 +0530

    types: refactor types

--- gnome-shell/global.d.ts
+++ gnome-shell/global.d.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 declare function log(message: any): void;
+declare function logError(err: Error, message?: string): void;
 declare interface IExtension {
 	enable(): void,
 	disable(): void;
@@ -13,4 +14,10 @@ declare interface ISubExtension {
 
 declare interface Math {
 	clamp(num: number, min: number, max: number): number;
+}
+
+declare interface ExtensionMeta {
+	uuid: string,
+	'settings-schema'?: string,
+	'gettext-domain'?: string
 }
