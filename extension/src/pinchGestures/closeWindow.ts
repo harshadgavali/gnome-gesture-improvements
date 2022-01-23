77a11612140165308684eca695f938569fb9779a
Achim Stuy <achim.stuy@gmail.com>
2022-01-24 00:26:59 +0100

    fixup! Add preview animation for close current window

--- extension/src/pinchGestures/closeWindow.ts
+++ extension/src/pinchGestures/closeWindow.ts
@@ -7,23 +7,23 @@ import St from '@gi-types/st1';
 import { global, imports } from 'gnome-shell';
 
 import { TouchpadPinchGesture } from '../trackers/pinchTracker';
-import { easeActor, easeAdjustment } from '../utils/environment';
+import { easeActor } from '../utils/environment';
 import { VirtualKeyboard } from '../utils/keyboard';
 
 const Main = imports.ui.main;
 const Utils = imports.misc.util;
 
+declare type Type_TouchpadPinchGesture = typeof TouchpadPinchGesture.prototype;
+
 const ClosePreview = registerClass(
 	class ClosePreview extends St.Widget {
 		private _adjustment: St.Adjustment;
 		private _windowBox?: Meta.Rectangle;
-		
 
 		constructor() {
 			super({
 				reactive: false,
-				style_class: 'tile-preview',
-				style: 'border-radius: 8px',
+				style_class: 'close-window-preview',
 				visible: false,
 			});
 			this.connect('destroy', this._onDestroy.bind(this));
@@ -31,37 +31,28 @@ const ClosePreview = registerClass(
 			this._adjustment = new St.Adjustment({
 				actor: this,
 				value: 0,
-				lower: -1,
-				upper: 2,
+				lower: 0,
+				upper: 1,
 			});
 
 			this._adjustment.connect('notify::value', this._valueChanged.bind(this));
 		}
 
-		open(window: Meta.Window, currentProgress: number): boolean{
+		open(window: Meta.Window): boolean{
 			this._windowBox = window.get_frame_rect();
 
 			this.opacity = 0;
-			this._adjustment.value = currentProgress;
+			this._adjustment.value = 0;
 			this._valueChanged();
 			this.visible = true;
 			this.easeOpacity(255);
 			return true;
 		}
 
-		finish(duration: number, progress: number): void {
-
-			const callback = () => {
-
+		finish(): void {
+			this.easeOpacity(0, () => {
+				this.visible = false;
 				this._windowBox = undefined;
-			};
-
-			this.easeOpacity(0, () => this.visible = false);
-
-			easeAdjustment(this._adjustment, progress, {
-				duration: duration,
-				mode: Clutter.AnimationMode.EASE_IN_EXPO,
-				onStopped: callback,
 			});
 		}
 
@@ -92,7 +83,7 @@ const ClosePreview = registerClass(
 			easeActor(this as St.Widget, {
 				opacity: value,
 				duration: 150,
-				mode: Clutter.AnimationMode.EASE_IN_EXPO,
+				mode: Clutter.AnimationMode.EASE,
 				onStopped: () => {
 					if (callback)
 						callback();
@@ -107,8 +98,11 @@ const ClosePreview = registerClass(
 );
 
 export class CloseWindowExtension implements ISubExtension {
+	// Define initial progress as 0.5 to allow pinch in and pinch out.
+	readonly _initialProgress: number = 0.5;
+	
 	private _keyboard: VirtualKeyboard;
-	private _pinchTracker: typeof TouchpadPinchGesture.prototype;
+	private _pinchTracker: Type_TouchpadPinchGesture;
 	private _preview: typeof ClosePreview.prototype;
 	private _uiGroupAddedActorId: number;
 
@@ -130,10 +124,13 @@ export class CloseWindowExtension implements ISubExtension {
 
 	apply(): void {
 		this._pinchTracker.connect('begin', this.gestureBegin.bind(this));
+		this._pinchTracker.connect('update', this.gestureUpdate.bind(this));
 		this._pinchTracker.connect('end', this.gestureEnd.bind(this));
 	}
 
 	destroy(): void {
+		this._pinchTracker?.destroy();
+
 		if (this._uiGroupAddedActorId) {
 			Main.layoutManager.uiGroup.disconnect(this._uiGroupAddedActorId);
 			this._uiGroupAddedActorId = 0;
@@ -144,20 +141,26 @@ export class CloseWindowExtension implements ISubExtension {
 		this._pinchTracker?.destroy();
 	}
 
-	gestureBegin() {
+	gestureBegin(tracker: Type_TouchpadPinchGesture) {
 		const window = global.display.get_focus_window() as Meta.Window | null;
 		if (window) {
-			this._pinchTracker.confirmPinch(0, [0, 1], 0);
-			this._preview.open(window, 0);
+			tracker.confirmPinch(0, [0, 1], this._initialProgress);
+			this._preview.open(window);
 		}
 	}
 
-	_gestureUpdate(progress: number): void {
-		this._preview.adjustment.value = progress;
+	gestureUpdate(_tracker: unknown, progress: number): void {
+		// Convert progress to number between 0 (start) and 1 (end).
+		let relativeProgress: number;
+		if (this._initialProgress === 0)
+			relativeProgress = progress;
+		else
+			relativeProgress = Math.abs(this._initialProgress - progress) / this._initialProgress;
+		this._preview.adjustment.value = relativeProgress;
 	}
 
-	gestureEnd(duration: number, progress: number) {
-		this._preview.finish(duration, progress);
+	gestureEnd(_tracker: unknown, _duration: number, _progress: number) {
+		this._preview.finish();
 		this._keyboard.sendKeys(Clutter.KEY_Control_L, Clutter.KEY_w);
 	}
 }
