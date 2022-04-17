52a84fa5dff59f23767d4ddb3a029587a0912e32
Your Name <you@example.com>
2022-04-17 23:26:07 +0530

    forwardBack: Send different keyboard keys for different application

--- extension/common/settings.ts
+++ extension/common/settings.ts
@@ -1,9 +1,19 @@
 import Gio from '@gi-types/gio2';
+import GLib from '@gi-types/glib2';
 
 // define enum
 export enum PinchGestureType {
     NONE = 0,
     SHOW_DESKTOP = 1,
+}
+
+export enum ForwardBackKeyBinds {
+    DEFAULT = 0,
+    FORWARD_BACK = 1,
+    PAGE_UP_DOWN = 2,
+    RIGHT_LEFT = 3,
+    AUDIO_NEXT_PREV = 4,
+    TAB_NEXT_PREV = 5,
 }
 
 export type BooleanSettingsKeys =
@@ -12,6 +22,7 @@ export type BooleanSettingsKeys =
     'allow-minimize-window' |
     'follow-natural-scroll' |
     'enable-alttab-gesture' |
+    'enable-forward-back-gesture' |
     'enable-window-manipulation-gesture' |
     'default-overview-gesture-direction'
     ;
@@ -29,12 +40,16 @@ export type EnumSettingsKeys =
     'pinch-4-finger-gesture'
     ;
 
+export type MiscSettingsKeys = 
+    'forward-back-application-keyboard-shortcuts'
+    ;
 
 export type AllSettingsKeys =
     BooleanSettingsKeys |
     IntegerSettingsKeys |
     DoubleSettingsKeys |
-    EnumSettingsKeys
+    EnumSettingsKeys |
+    MiscSettingsKeys
     ;
 
 export type AllUIObjectKeys =
@@ -54,6 +69,15 @@ type SettingsEnumFunctions =
     Enum_Functions<'pinch-3-finger-gesture' | 'pinch-4-finger-gesture', PinchGestureType>
     ;
 
+type Misc_Functions<K extends MiscSettingsKeys, T extends string> = {
+    get_value(key: K): GLib.Variant<T>;
+    set_value(key: K, value: GLib.Variant<T>): void;
+}
+
+type SettingsMiscFunctions = 
+    Misc_Functions<'forward-back-application-keyboard-shortcuts', 'a{s(ib)}'>
+    ;
+
 export type GioSettings =
     Omit<Gio.Settings, KeysThatStartsWith<keyof Gio.Settings, 'get_' | 'set_'>> &
     {
@@ -62,5 +86,6 @@ export type GioSettings =
         get_double(key: DoubleSettingsKeys): number;
         set_double(key: DoubleSettingsKeys, value: number): void;
     } &
-    SettingsEnumFunctions
+    SettingsEnumFunctions &
+    SettingsMiscFunctions
     ;
--- extension/src/utils/keyboard.ts
+++ extension/src/utils/keyboard.ts
@@ -1,17 +1,53 @@
 import Clutter from '@gi-types/clutter8';
+import GLib from '@gi-types/glib2';
+
+const DELAY_BETWEEN_KEY_PRESS = 10; // ms
+const timeoutIds = new Set<number>();
 
 class VirtualKeyboard {
 	private _virtualDevice: Clutter.VirtualInputDevice;
-	
+
 	constructor() {
 		const seat = Clutter.get_default_backend().get_default_seat();
 		this._virtualDevice = seat.create_virtual_device(Clutter.InputDeviceType.KEYBOARD_DEVICE);
 	}
 
-	sendKeys(...keys: number[]) {
-		const currentTime = Clutter.get_current_event_time();
-		keys.forEach(key => this._virtualDevice.notify_keyval(currentTime, key, Clutter.KeyState.PRESSED));
-		keys.reverse().forEach(key => this._virtualDevice.notify_keyval(currentTime, key, Clutter.KeyState.RELEASED));
+	sendKeys(keys: number[]) {
+		log(`sending keys: ${keys}`);
+
+		// keyEvents are stored in revere order so first event can be just popped
+		const keyEvents: [number, Clutter.KeyState][] = [];
+		keys.forEach(key => keyEvents.push([key, Clutter.KeyState.RELEASED]));
+		keys.reverse().forEach(key => keyEvents.push([key, Clutter.KeyState.PRESSED]));
+
+		let timeoutId = GLib.timeout_add(
+			GLib.PRIORITY_DEFAULT,
+			DELAY_BETWEEN_KEY_PRESS,
+			() => {
+				const keyEvent = keyEvents.pop();
+				if (keyEvent !== undefined)
+					this._sendKey(...keyEvent);
+				
+				if (keyEvents.length === 0) {
+					timeoutIds.delete(timeoutId);
+					timeoutId = 0;
+					return GLib.SOURCE_REMOVE;
+				}
+
+				return GLib.SOURCE_CONTINUE;
+			},
+		);
+
+		if (timeoutId)
+			timeoutIds.add(timeoutId);
+	}
+
+	private _sendKey(keyval: number, keyState: Clutter.KeyState) {
+		this._virtualDevice.notify_keyval(
+			Clutter.get_current_event_time() * 1000,
+			keyval,
+			keyState,
+		);
 	}
 }
 
@@ -24,5 +60,7 @@ export function getVirtualKeyboard() {
 }
 
 export function extensionCleanup() {
+	timeoutIds.forEach(id => GLib.Source.remove(id));
+	timeoutIds.clear();
 	_keyboard = undefined;
 }
