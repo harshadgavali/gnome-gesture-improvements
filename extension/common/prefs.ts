e29d4ab0725ad3488b8a0c0df2fa2ae735a76a29
Harshad Gavali <harshadgavali@outlook.com>
2022-04-20 01:48:31 +0530

    prefs: Always show minimize gesture option

--- extension/common/prefs.ts
+++ extension/common/prefs.ts
@@ -4,7 +4,6 @@ import Gtk from '@gi-types/gtk4';
 import Gdk from '@gi-types/gdk4';
 import Adw from '@gi-types/adw1';
 import { AllUIObjectKeys, BooleanSettingsKeys, DoubleSettingsKeys, EnumSettingsKeys, GioSettings, IntegerSettingsKeys } from './settings';
-import { CanEnableMinimizeGesture } from './utils/prefUtils';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { getAppKeybindingGesturePrefsPage } from './appGestures';
@@ -63,15 +62,6 @@ function display_in_log_scale(key: DoubleSettingsKeys, label_key: AllUIObjectKey
 	scale.set_value(initialValue);
 }
 
-/** Show button to enable minimize gesture, returns whether button was shown */
-function showEnableMinimizeButton(key: BooleanSettingsKeys, row_key: AllUIObjectKeys, settings: GioSettings, builder: GtkBuilder) {
-	const row = builder.get_object<Gtk.ListBoxRow>(row_key);
-	row.visible = settings.get_boolean(key) || CanEnableMinimizeGesture();
-	if (row.visible)
-		bind_boolean_value(key, settings, builder);
-	return row.visible;
-}
-
 /**
  * Binds preference widgets and settings keys
  * @param builder builder object for preference widgets
@@ -91,8 +81,7 @@ function bindPrefsSettings(builder: GtkBuilder, settings: Gio.Settings) {
 
 	bind_boolean_value('enable-alttab-gesture', settings, builder);
 	bind_boolean_value('enable-window-manipulation-gesture', settings, builder);
-
-	showEnableMinimizeButton('allow-minimize-window', 'allow-minimize-window_box-row', settings, builder);
+	bind_boolean_value('allow-minimize-window', settings, builder);
 
 	bind_combo_box('pinch-3-finger-gesture', settings, builder);
 	bind_combo_box('pinch-4-finger-gesture', settings, builder);
